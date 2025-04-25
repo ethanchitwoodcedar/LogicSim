e7dbbdd3c00256d0704aeacd8174e95ab2920ca5
ethanchitwoodcedar <ethanchitwood@cedarville.edu>
2025-04-25 00:23:17 -0400

    Update Gate.h

--- LogicSim/Gate.h
+++ LogicSim/Gate.h
@@ -17,17 +17,18 @@ private:
     Wire* input1;
     Wire* input2;
     Wire* output;
-    int AND() const;
-    int OR() const;
-    int XOR() const;
-    int NAND() const;
-    int NOR() const;
-    int XNOR() const;
-    int NOT() const;
+    int AND(int time) const;
+    int OR(int time) const;
+    int XOR(int time) const;
+    int NAND(int time) const;
+    int NOR(int time) const;
+    int XNOR(int time) const;
+    int NOT(int time) const;
 public:
     Gate(int, int, Wire*, Wire*, Wire*);
     int getDelay() const { return gateDelay; }
     Wire* getInput(int inputNum) const { return (inputNum == 0) ? input1 : input2; }
     Wire* getOutput() const { return output; }
-    int evaluate() const; // 1 is true. 0 is false. -1 is undefined.
+    int evaluate(int time) const; // 1 is true. 0 is false. -1 is undefined.
 };
+
